09707018f38a8fac20de1e0c5458ed39ef817bcd
OptimusJc <optimus.prime.jc@gmail.com>
2022-10-05 17:41:50 +0300

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+// core imports
+import { Outlet } from "react-router-dom";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+// style and component imports
+import Container from "react-bootstrap/Container";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+
+const App = () => {
+	return (
+		<Container fluid>
+			<Header />
+			<Outlet />
+			<Footer />
+		</Container>
+	);
+};
 
 export default App;
---
+++ src/components/Footer.js
@@ -0,0 +1,7 @@
+import styled from "styled-components";
+
+const Footer = () => {
+	return <h1>This is the footer section</h1>;
+};
+
+export default Footer;
