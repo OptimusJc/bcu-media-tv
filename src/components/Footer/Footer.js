d34b95b937fdadd81b992d1bafec55a473e7c71e
OptimusJc <optimus.prime.jc@gmail.com>
2022-10-06 23:20:56 +0300

    style: Add footer section

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,5 +1,33 @@
+import { Container } from "react-bootstrap";
+
+import { FaFacebookF, FaTwitter, FaYoutube } from "react-icons/fa";
+
 const Footer = () => {
-	return <h1>This is the footer section</h1>;
+	return (
+		<footer>
+			<Container fluid>
+				<div className="footer-content">
+					<div className="copyright">
+						<p>@copy 2022 - BCU Tv</p>
+					</div>
+					<div className="contact">
+						<a href="#" className="contact_us">
+							Contact Us
+						</a>
+						<a href="#" className="facebook">
+							<FaFacebookF />
+						</a>
+						<a href="#" className="twitter">
+							<FaTwitter />
+						</a>
+						<a href="#" className="youtube">
+							<FaYoutube />
+						</a>
+					</div>
+				</div>
+			</Container>
+		</footer>
+	);
 };
 
 export default Footer;
