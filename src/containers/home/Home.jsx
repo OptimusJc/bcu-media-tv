bd106c3a380e7cc8ddc30544a02ccdfbf4fc93ac
OptimusJc <optimus.prime.jc@gmail.com>
2022-10-08 04:41:54 +0300

    style: Add the hero section

--- src/containers/home/Home.jsx
+++ src/containers/home/Home.jsx
@@ -1,5 +1,41 @@
+import { Container, Row, Col } from "react-bootstrap";
+import backgroundImage from "../../assets/home_background.png";
+
+import ReactPlayer from "react-player/lazy";
+
 const Home = () => {
-	return <h1>This is the home page!!!</h1>;
+	const VIDEO_URL = "https://www.youtube.com/watch?v=p49ifHvi25A&t=137s";
+
+	return (
+		<section className="home">
+			<img src={backgroundImage} alt="Home background" />
+			<Container fluid>
+				<Row>
+					<Col>
+						<div className="hero_container">
+							<div className="hero_title">
+								<span></span>
+								<h1>Body of Christ United Global Internation Ministries Tv</h1>
+								<button type="button" className="btn btn-warning">
+									Watch
+								</button>
+							</div>
+							<div className="hero_video">
+								<ReactPlayer
+									url={VIDEO_URL}
+									controls
+									playsinline
+									width="100%"
+									height="200px"
+								/>
+							</div>
+						</div>
+					</Col>
+				</Row>
+				<Row style={{ height: "500px" }}></Row>
+			</Container>
+		</section>
+	);
 };
 
 export default Home;
